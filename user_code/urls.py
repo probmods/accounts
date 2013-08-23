19ebe0766151ea27375ef2f4a904934b31d3a8af
Long Ouyang <longouyang@gmail.com>
2013-08-23 15:44:01 -0700

    Fix slash error.
    Urls were getting built like /codesimple-flip/, rather than /code/simple-flip

--- user_code/urls.py
+++ user_code/urls.py
@@ -3,6 +3,5 @@ from user_code import views
 from user_code.views import user_exercise
 
 urlpatterns = patterns('',
-    url(r'(?P<string>[-\w]+)/$', user_exercise, name ='user_exercise'),
-        
+    url(r'/(?P<string>[-\w]+)$', user_exercise, name ='user_exercise'), 
 )
